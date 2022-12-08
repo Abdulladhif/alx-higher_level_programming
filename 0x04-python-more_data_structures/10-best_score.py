93dbd5df07910a91640467e6d50090b54d064748
Abdulladhif <sabirbaari@gmail.com>
2022-12-07 20:02:23 -0800

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,4 @@
 # Returns a key with the biggest integer value.
 
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
-    max_val = max(a_dictionary, key= lambda val: a_dictionary[val])
-    return max_val
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
