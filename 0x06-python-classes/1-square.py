74d889ba9dc6847f227c513a10d659db8ca17c9b
Abdulladhif <sabirbaari@gmail.com>
2022-12-20 11:38:16 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """ Initialize a square
+        @size private instance attribute
+        """
+        self.__size = size
